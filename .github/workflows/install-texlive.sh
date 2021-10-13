2e13d2c69b5ac1d710d30836382b2c3a6a7ff78f
XiaoqiangWu <39110174+wuxiaoqiang12@users.noreply.github.com>
2021-10-13 15:30:08 +0800

    Update install-texlive.sh

--- .github/workflows/install-texlive.sh
+++ .github/workflows/install-texlive.sh
@@ -24,3 +24,26 @@ tlmgr install l3build \
     listings glossaries mfirstuc textcase xfor datatool tracklang pdflscape rsfs\
     txfonts xecjk tex-gyre newpx mhchem makecell xstring graphbox wrapfig titlesec \
     tcolorbox fontawesome
+    
+if [ ! -d $HOME/.fonts ]; then
+    mkdir $HOME/.fonts;
+fi
+
+wget https://github.com/wuxiaoqiang12/algorithm_learning_acwing/raw/main/fonts/%E6%96%B9%E6%AD%A3%E4%B8%AD%E7%AD%89%E7%BA%BF_GBK.ttf -O $HOME/.fonts/%E6%96%B9%E6%AD%A3%E4%B8%AD%E7%AD%89%E7%BA%BF_GBK.ttf
+wget https://github.com/wuxiaoqiang12/algorithm_learning_acwing/raw/main/fonts/%E6%96%B9%E6%AD%A3%E4%B9%A6%E5%AE%8B_GBK.ttf -O $HOME/.fonts/%E6%96%B9%E6%AD%A3%E4%B9%A6%E5%AE%8B_GBK.ttf
+wget https://github.com/wuxiaoqiang12/algorithm_learning_acwing/raw/main/fonts/%E6%96%B9%E6%AD%A3%E4%BB%BF%E5%AE%8B_GBK.ttf -O $HOME/.fonts/%E6%96%B9%E6%AD%A3%E4%BB%BF%E5%AE%8B_GBK.ttf
+wget https://github.com/wuxiaoqiang12/algorithm_learning_acwing/raw/main/fonts/%E6%96%B9%E6%AD%A3%E5%B0%8F%E6%A0%87%E5%AE%8B_GBK.ttf -O $HOME/.fonts/%E6%96%B9%E6%AD%A3%E5%B0%8F%E6%A0%87%E5%AE%8B_GBK.ttf
+wget https://github.com/wuxiaoqiang12/algorithm_learning_acwing/raw/main/fonts/%E6%96%B9%E6%AD%A3%E6%A5%B7%E4%BD%93_GBK.ttf -O $HOME/.fonts/%E6%96%B9%E6%AD%A3%E6%A5%B7%E4%BD%93_GBK.ttf
+wget https://github.com/wuxiaoqiang12/algorithm_learning_acwing/raw/main/fonts/%E6%96%B9%E6%AD%A3%E9%BB%91%E4%BD%93_GBK.ttf -O $HOME/.fonts/%E6%96%B9%E6%AD%A3%E9%BB%91%E4%BD%93_GBK.ttf
+
+if [ ! -d $HOME/.fonts/Myriad\ Pro ]; then
+    mkdir $HOME/.fonts/Myriad\ Pro;
+fi
+
+wget https://github.com/wuxiaoqiang12/algorithm_learning_acwing/raw/main/fonts/Myriad%20Pro/MyriadPro-Bold.otf -O $HOME/.fonts/Myriad\ Pro/MyriadPro-Bold.otf
+wget https://github.com/wuxiaoqiang12/algorithm_learning_acwing/raw/main/fonts/Myriad%20Pro/MyriadPro-BoldIt.otf -O $HOME/.fonts/Myriad\ Pro/MyriadPro-BoldIt.otf
+wget https://github.com/wuxiaoqiang12/algorithm_learning_acwing/raw/main/fonts/Myriad%20Pro/MyriadPro-It.otf -O $HOME/.fonts/Myriad\ Pro/MyriadPro-It.otf
+wget https://github.com/wuxiaoqiang12/algorithm_learning_acwing/raw/main/fonts/Myriad%20Pro/MyriadPro-Regular.otf -O $HOME/.fonts/Myriad\ Pro/MyriadPro-Regular.otf
+
+
+fc-cache -f $HOME/.fonts/
